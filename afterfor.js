55bed4fa25bb95c07741c57e4bf1d47350ac6028
kavyamk025 <kavyamk025@gmail.com>
2026-03-24 23:08:45 +0530

    first commit

---
+++ afterfor.js
@@ -0,0 +1,35 @@
+// let n = 1234321
+// let n1=n
+// let rev=0
+// while (n >0) {
+//     let digit=n%10
+//     rev=rev*10+digit
+//     n=Math.floor(n/10)
+
+// }
+// if (rev===n1){
+//     console.log("Yes palindrome");
+// }
+// else{
+//     console.log("Not a palindrome");
+// }
+// //armstrong number
+// let nu=371
+// let str=String(nu)
+// let i=0
+// let sum=0
+// let temp=0
+// while(i<str.length){
+//     temp=Number(str[i])
+//     sum+=temp**3
+//     i++
+// }
+// if(sum===nu){
+//     console.log("yes armstrong number");
+// }
+// else{
+//     console.log("not  aarmstrong number");
+// }
+
+//foreach
+//map,reduce,filter,selectors,id,class,tags,settimeout,settimeinterval,promise,fetch=>display,async and await
